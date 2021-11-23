666ce2a56f1d59274db52c89d432166bdd2e2cbf
Jheyson Herrera <herrerajheyson@gmail.com>
2021-11-23 14:47:15 -0500

    Cambios extras en menú visitante

--- resources/views/layouts/navbars/navs/guest.blade.php
+++ resources/views/layouts/navbars/navs/guest.blade.php
@@ -27,7 +27,7 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
                     <a class="nav-link nav-link-icon" href="{{ route('home') }}">
-                        <i class="ni ni-planet"></i>
+                        <i class="ni ni-cart"></i>
                         <span class="nav-link-inner--text">{{ __('Tienda') }}</span>
                     </a>
                 </li>
@@ -41,12 +41,6 @@
                     <a class="nav-link nav-link-icon" href="{{ route('login') }}">
                         <i class="ni ni-key-25"></i>
                         <span class="nav-link-inner--text">{{ __('Iniciar Sesión') }}</span>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-icon" href="{{ route('profile.edit') }}">
-                        <i class="ni ni-single-02"></i>
-                        <span class="nav-link-inner--text">{{ __('Perfil') }}</span>
                     </a>
                 </li>
             </ul>
