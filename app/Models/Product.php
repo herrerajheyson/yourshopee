1ba0bd13d373b0e74cd77004b48a443d4e4e475a
Jheyson Herrera <herrerajheyson@gmail.com>
2021-11-24 18:18:56 -0500

    Configuración del modelo de productos

---
+++ app/Models/Product.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Product extends Model
+{
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'sku',
+        'name',
+        'brand',
+        'price',
+        'amount',
+        'discount',
+        'reference',
+        'image',
+        'description',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'sku',
+    ];
+
