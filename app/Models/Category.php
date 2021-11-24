ababde45f23b99d1220b7d4e52aef15deb019366
Jheyson Herrera <herrerajheyson@gmail.com>
2021-11-24 13:35:47 -0500

    Configuración del modelo de categorías

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Category extends Model
+{
+    use HasFactory;
+    use Notifiable;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+}
